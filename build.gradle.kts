10b6309e2d948b24b9d6032f62a0d77e91deb302
iwasafi9046-afk <ykoh9046@gmail.com>
2026-06-15 07:52:31 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,7 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-  id("com.android.application") version "9.1.1" apply false
-  id("org.jetbrains.kotlin.plugin.compose") version "2.2.10" apply false
-  id("com.google.devtools.ksp") version "2.3.5" apply false
-  id("io.github.takahirom.roborazzi") version "1.59.0" apply false
-  id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
+    id("com.android.application") version "8.0.2" apply false
+    id("com.android.library") version "8.0.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.20" apply false
+    id("com.google.devtools.ksp") version "1.8.20-1.0.11" apply false
 }
